52b51cdb4a25f260feee32bf8786873c0bf7b4c2
CJY <375564567@qq.com>
2018-01-29 10:41:50 +0800

    - 更新 - 加入 prettier ，此次提交测试 precommit 功能

--- webpack/dll/webpack.config.js
+++ webpack/dll/webpack.config.js
@@ -1,39 +1,39 @@
 const path = require('path')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const {
-  optimize: {
-    UglifyJsPlugin
-  } = {},
-  DllPlugin, DefinePlugin, IgnorePlugin
+  optimize: { UglifyJsPlugin } = {},
+  DllPlugin,
+  DefinePlugin,
+  IgnorePlugin
 } = require('webpack')
 
 module.exports = {
   entry: require('./entry'),
   output: {
     path: path.resolve(__dirname, '../../dist/lib'),
-    filename: '[name].[chunkhash:8].js',
-    library: '[name]_[chunkhash:8]'
+    filename: '[name].[chunkhash:6].js',
+    library: '[name]_[chunkhash:6]'
   },
   plugins: [
-    new CleanWebpackPlugin([
-      'manifest',
-      path.resolve(__dirname, '../../dist/lib')
-    ], {
-      allowExternal: true,
-      beforeEmit: true
-    }),
+    new CleanWebpackPlugin(
+      ['manifest', path.resolve(__dirname, '../../dist/lib')],
+      {
+        allowExternal: true,
+        beforeEmit: true
+      }
+    ),
     // 忽略国际化部分以减小 moment.js 体积，参考：https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     new IgnorePlugin(/^\.\/locale$/, /moment$/),
     // 环境变量设置为生产模式以减小 react 体积，参考：https://reactjs.org/docs/add-react-to-an-existing-app.html#development-and-production-versions
     new DefinePlugin({
       'process.env': {
-          NODE_ENV: JSON.stringify('production'),
+        NODE_ENV: JSON.stringify('production')
       }
     }),
     new DllPlugin({
       path: path.resolve(__dirname, './manifest/[name].json'),
-      name: '[name]_[chunkhash:8]',
-      context: __dirname,
+      name: '[name]_[chunkhash:6]',
+      context: __dirname
     }),
     new UglifyJsPlugin({
       compress: {
