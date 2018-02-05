2bfbd7df0ea1218e98c0e31380179c2c43c26461
CJY <375564567@qq.com>
2018-02-05 10:55:20 +0800

    - 更新 - 没什么改动

--- dev/server.js
+++ dev/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express'),
   app = express()
 
@@ -7,6 +8,11 @@ app.use(
     extensions: ['html']
   })
 )
+
+// app.get('*', function (request, response){
+//   console.log(request)
+//   response.sendFile(path.resolve(__dirname, '../dist', `${request.originalUrl.split('/')[2]}.html`))
+// })
 
 app.listen(10001, () => {
   console.log('Server is Running at http://localhost:10001')
