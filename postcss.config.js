da375eaa11b3c77a503f555ddbfb6606bf0ce955
CJY <375564567@qq.com>
2018-05-08 17:22:51 +0800

    - 更新 - 设计稿尺寸调整为 750px

--- postcss.config.js
+++ postcss.config.js
@@ -11,7 +11,7 @@ module.exports = ({ file = {}, options = {}, env }) => ({
   plugins: [
     require('postcss-modules-values')(),
     require('postcss-px-to-viewport')({
-      viewportWidth: 720, // 视窗的宽度，对应的是我们设计稿的宽度，一般是750
+      viewportWidth: 750, // 视窗的宽度，对应的是我们设计稿的宽度，一般是750
       // viewportHeight: 1280, // 视窗的高度，根据750设备的宽度来指定，一般指定1334，也可以不配置
       unitPrecision: 3, // 指定`px`转换为视窗单位值的小数位数（很多时候无法整除）
       viewportUnit: 'vw', // 指定需要转换成的视窗单位，建议使用vw
