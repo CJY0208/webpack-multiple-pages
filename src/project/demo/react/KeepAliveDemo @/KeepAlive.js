c0cb6d50437944a860f1b9bfdcaa41efc6de8b54
CJY <375564567@qq.com>
2019-08-07 18:20:53 +0800

    持续开发 keep-alive

--- src/project/demo/react/KeepAliveDemo @/KeepAlive.js
+++ src/project/demo/react/KeepAliveDemo @/KeepAlive.js
@@ -6,24 +6,21 @@ import React, {
   createRef
 } from 'react'
 
-import { run, get } from '@helpers'
+import { run, get, isObject } from '@helpers'
 
 const nextTick = async func => {
   await Promise.resolve()
   run(func)
 }
 
-export class AliveStore extends Component {
-  static instance
-  static setCache = (...args) => run(AliveStore.instance, 'setCache', ...args)
-  static getCache = (...args) => run(AliveStore.instance, 'getCache', ...args)
-  static getCacheNode = (...args) =>
-    run(AliveStore.instance, 'getCacheNode', ...args)
+let Store
 
+export class AliveStore extends Component {
   constructor(props) {
     super(props)
 
-    AliveStore.instance = this
+    Store = this
+    window.Store = this
   }
 
   cacheNodes = {}
@@ -42,9 +39,9 @@ export class AliveStore extends Component {
               component: (
                 <div
                   key={key}
+                  className={key}
                   ref={node => {
                     if (!this.cacheNodes[key] && node) {
-                      node.parentNode.removeChild(node)
                       this.cacheNodes[key] = get(node, 'children.0')
                     }
                   }}
@@ -64,11 +61,25 @@ export class AliveStore extends Component {
 
   getCache = key => this.state.cache[key]
   getCacheNode = key => this.cacheNodes[key]
+  removeCache = key => {
+    const node = this.cacheNodes[key]
+
+    if (node.cached) {
+      let cache = { ...this.state.cache }
+      delete cache[key]
+      delete this.cacheNodes[key]
+      this.setState({ cache })
+    }
+  }
 
   render() {
     const { cache } = this.state
 
-    return Object.values(cache).map(({ component, ...rest }) => component)
+    return (
+      <div id="KeepAliveStore" style={{ display: 'none' }}>
+        {Object.values(cache).map(({ component, ...rest }) => component)}
+      </div>
+    )
   }
 }
 
@@ -82,18 +93,30 @@ export default class KeepAlive extends Component {
   init = async () => {
     const { children, name } = this.props
 
-    const node = await AliveStore.setCache(name, {
-      children: React.cloneElement(children, {
-        // ref: (instance) => {
-        //   // console.log(instance)
+    window.children = children
 
-        //   instance.componentDidRecover()
-        //   children.ref(instance)
-        // },
+    let instance
+    const node = await Store.setCache(name, {
+      children: React.cloneElement(children, {
+        ref: ref => {
+          instance = ref
+          run(children, 'ref', ref)
+          if (isObject(children.ref)) {
+            children.ref.current = ref
+          }
+        },
         injectKeepAliveCycles: val => console.log(val)
       })
     })
 
+    if (!node.inited) {
+      node.inited = true
+    } else {
+      run(instance, 'componentDidRecover')
+    }
+
+    node.instance = instance
+    node.cached = false
     this.parentNode = this.placeholder.parentNode
     this.parentNode.replaceChild(node, this.placeholder)
   }
@@ -102,7 +125,10 @@ export default class KeepAlive extends Component {
     console.log('componentWillUnmount')
 
     const { name } = this.props
-    const node = AliveStore.getCacheNode(name)
+    const node = Store.getCacheNode(name)
+
+    node.cached = true
+    run(node, 'instance.componentWillCache')
 
     this.parentNode.replaceChild(this.placeholder, node)
   }
