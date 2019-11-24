f73e7f556bb4d338f35dd2e7af888b46f0e3206f
CJY <375564567@qq.com>
2019-11-24 15:12:56 +0800

    resizable 实现

--- src/project/demo/react/resizableDemo @/Resizable/index.js
+++ src/project/demo/react/resizableDemo @/Resizable/index.js
@@ -1,4 +1,147 @@
-import ResizableX from './components/ResizableX'
-import ResizableContent from './components/ResizableContent'
+import React, { useState, useEffect, useRef } from 'react'
 
-export { ResizableX, ResizableContent }
+import { clamp } from '@helpers'
+
+import './style.m.scss'
+
+const percentReg = /\%$/
+
+export function ResizableX({
+  children: [leftContent, rightContent],
+  initialValue = '50%',
+  min = 0,
+  max = '100%',
+  barSize = 6
+}) {
+  const [pos, setPos] = useState(0)
+  const [resizing, setResizing] = useState(false)
+  const container = useRef()
+  const calcValue = value =>
+    percentReg.test(value)
+      ? parseFloat(value) * container.current.offsetWidth / 100
+      : value
+  const fixPos = pos => clamp(pos - 1, calcValue(min), calcValue(max))
+  const resize = e => {
+    if (!resizing) {
+      return
+    }
+
+    const nextPos = fixPos(
+      e.clientX - container.current.getBoundingClientRect().x
+    )
+
+    if (!Number.isNaN(nextPos)) {
+      setPos(nextPos)
+    }
+  }
+
+  useEffect(() => {
+    setPos(fixPos(calcValue(initialValue)))
+  }, [])
+
+  useEffect(
+    () => {
+      const resizeListener = () => {
+        setPos(fixPos(pos))
+      }
+      window.addEventListener('resize', resizeListener)
+
+      return () => window.removeEventListener('resize', resizeListener)
+    },
+    [pos]
+  )
+
+  return (
+    <div
+      ref={container}
+      className={`resizable resizable-x ${resizing ? 'resizing' : ''}`}
+      onMouseUp={() => setResizing(false)}
+      onMouseMove={resize}
+    >
+      <div
+        className="content"
+        style={{
+          width: pos
+        }}
+      >
+        {leftContent}
+      </div>
+      <div className="content auto">{rightContent}</div>
+      <div
+        className="bar"
+        style={{ left: pos, width: barSize }}
+        onMouseDown={() => setResizing(true)}
+      />
+    </div>
+  )
+}
+
+export function ResizableY({
+  children: [topContent, bottomContent],
+  initialValue = '50%',
+  min = 0,
+  max = '100%',
+  barSize = 6
+}) {
+  const [pos, setPos] = useState(0)
+  const [resizing, setResizing] = useState(false)
+  const container = useRef()
+  const calcValue = value =>
+    percentReg.test(value)
+      ? parseFloat(value) * container.current.offsetHeight / 100
+      : value
+  const fixPos = pos => clamp(pos - 1, calcValue(min), calcValue(max))
+  const resize = e => {
+    if (!resizing) {
+      return
+    }
+
+    const nextPos = fixPos(
+      e.clientY - container.current.getBoundingClientRect().y
+    )
+
+    if (!Number.isNaN(nextPos)) {
+      setPos(nextPos)
+    }
+  }
+
+  useEffect(() => {
+    setPos(fixPos(calcValue(initialValue)))
+  }, [])
+
+  useEffect(
+    () => {
+      const resizeListener = () => {
+        setPos(fixPos(pos))
+      }
+      window.addEventListener('resize', resizeListener)
+
+      return () => window.removeEventListener('resize', resizeListener)
+    },
+    [pos]
+  )
+
+  return (
+    <div
+      ref={container}
+      className={`resizable resizable-y ${resizing ? 'resizing' : ''}`}
+      onMouseUp={() => setResizing(false)}
+      onMouseMove={resize}
+    >
+      <div
+        className="content"
+        style={{
+          height: pos
+        }}
+      >
+        {topContent}
+      </div>
+      <div className="content auto">{bottomContent}</div>
+      <div
+        className="bar"
+        style={{ top: pos, height: barSize }}
+        onMouseDown={() => setResizing(true)}
+      />
+    </div>
+  )
+}
--- src/project/demo/react/resizableDemo @/index.js
+++ src/project/demo/react/resizableDemo @/index.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import { ResizableX, ResizableContent } from './Resizable'
+import { ResizableX, ResizableY } from './Resizable'
 import Content from './Content'
 
 render(
-  <div>
-    <ResizableX initialValue={20} min={30} max={60}>
-      <ResizableContent>
+  <div style={{ height: '100%' }}>
+    <ResizableY min="30%" max="70%" barSize={20}>
+      <ResizableX min="30%" max="70%" barSize={20}>
         <Content>Content 1</Content>
-      </ResizableContent>
-      <ResizableContent>
         <Content>Content 2</Content>
-      </ResizableContent>
-    </ResizableX>
+      </ResizableX>
+      <ResizableX min="30%" max="70%" barSize={20}>
+        <Content>Content 3</Content>
+        <Content>Content 4</Content>
+      </ResizableX>
+    </ResizableY>
   </div>,
   document.getElementById('root')
 )
